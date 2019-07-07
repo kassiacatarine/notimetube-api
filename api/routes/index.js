27417ae88c19d57f00b911ba815a0426d003b909
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-07-07 16:51:34 -0300

    Adicionando rotas inicial

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.status(204).json('NoTimeTube API');
+  res.send('NoTimeTube API');
+  // res.status(204).json('NoTimeTube API');
 });
 
 module.exports = router;
